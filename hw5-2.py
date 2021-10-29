b6d5e51e00c0189ea749626d25dd16f6a37bccde
p22cgussen <p22cgussen@fairfieldprep.org>
2021-10-29 11:14:42 -0400

    complete hw5-2

---
+++ hw5-2.py
@@ -0,0 +1,35 @@
+# Author CCG 10/29/21
+
+import math
+
+# Question 1
+print(math.isqrt(529))
+
+# Question 2
+print(math.sin(0))
+
+# Question 3
+x = math.pi
+print(math.cos(2 * x))
+
+# Question 4
+print(math.pow(2.5, 5))
+
+# Question 5
+x = math.e
+print(math.log(x))
+
+# Question 6
+print(math.pow(math.e, 4))
+
+# Question 7
+print(math.ceil(-12.9))
+
+# Question 8
+print(math.floor(7.25))
+
+# Question 9
+print(math.log(1000) * 2)
+
+# Question 10
+print(math.radians(180))
